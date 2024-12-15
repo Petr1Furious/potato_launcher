a5f6064b4363249b9348939a2c23bd4d63932d23
Nikita Denisov <qwertyzivitq@gmail.com>
2024-12-15 20:39:05 +0300

    some improvements of windows command execution

--- shared/src/utils.rs
+++ shared/src/utils.rs
@@ -6,19 +6,20 @@ pub async fn exec_custom_command(command: &str) -> anyhow::Result<()> {
     exec_custom_command_in_dir(command, &Path::new(".")).await
 }
 
-// Костыль под винду
 pub async fn exec_custom_command_in_dir(command: &str, dir: &Path) -> anyhow::Result<()> {
     let parts = shell_words::split(command)?;
     let mut modified_parts: Vec<String> = parts.iter().map(|part| part.clone()).collect();
 
-    // Удаляем первые 3 символа у первого и третьего элемента
-    if modified_parts.len() > 0 {
-        modified_parts[0] = modified_parts[0][3..].to_string(); // Удаляем первые 3 символа у первого элемента
+    // Костыль под винду для созданиия новых инстансов
+    if env::consts::OS == "windows" && modified_parts[0][..3].to_string() == "\\?\\" && modified_parts[2][..3].to_string() == "\\?\\" {
+        // Удаляем первые 3 символа у первого и третьего элемента
+        if modified_parts.len() > 0 {
+            modified_parts[0] = modified_parts[0][3..].to_string(); // Удаляем первые 3 символа у первого элемента
+        }
+        if modified_parts.len() > 2 {
+            modified_parts[2] = modified_parts[2][3..].to_string(); // Удаляем первые 3 символа у третьего элемента
+        }
     }
-    if modified_parts.len() > 2 {
-        modified_parts[2] = modified_parts[2][3..].to_string(); // Удаляем первые 3 символа у третьего элемента
-    }
-
 
     let mut cmd = tokio::process::Command::new(&modified_parts[0]);
     if modified_parts.len() > 1 {
